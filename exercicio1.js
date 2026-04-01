66083e3b1e6846adfba625a726060abed10b07b3
Thiago Oliveira <thiagotg4facul@gmail.com>
2026-04-01 08:22:10 -0300

    Implements gravitational calculation and adds image gallery functionality

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,12 @@
-// Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
-// Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+let botaoCalcular = document.querySelector('#calcular');
+
+botaoCalcular.addEventListener('click', function () {
+    let G = document.querySelector('#constante').value;
+    let M1 = document.querySelector('#massa1').value;
+    let M2 = document.querySelector('#massa2').value;
+    let d = document.querySelector('#distancia').value;
+
+    let F = (G * M1 * M2) / (d * d);
+    document.querySelector('#resultado').value = F;
+});
--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,14 @@
-// Faça o exercício dos PARÁGRAFOS aqui
-// Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+let botoes = document.querySelectorAll('.botao-expandir-retrair');
+
+botoes.forEach(function (botao) {
+    botao.addEventListener('click', function (e) {
+        let paragrafo = e.currentTarget.parentElement;
+        paragrafo.classList.toggle('expandido');
+
+        if (paragrafo.classList.contains('expandido')) {
+            e.currentTarget.innerHTML = '-';
+        } else {
+            e.currentTarget.innerHTML = '+';
+        }
+    });
+});
